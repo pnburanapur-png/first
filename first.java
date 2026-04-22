d31ae0f4b3ef70b4aee3561a5819c6c73eee2cc8
pnburanapur-png <pnburanapur@gmail.com>
2026-04-22 08:59:39 +0530

    Update first.java

--- first.java
+++ first.java
@@ -2,5 +2,6 @@ class first{
     public static void main(String []args){
         System.out.println("Hello wrold!");
         System.out.println("Pavitra");
+        System.out.prinln("World")
     }
 }
